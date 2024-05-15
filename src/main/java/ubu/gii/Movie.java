3a26ac56adf4d9e2b9679eca1b716726782b791f
bbp1002 <bbp1002@alu.ubu.es>
2024-05-15 15:36:31 +0200

    #2 Move Rental.getFrequentRenterPoint Rental to Movie

--- src/main/java/ubu/gii/Movie.java
+++ src/main/java/ubu/gii/Movie.java
@@ -55,4 +55,13 @@ public class Movie {
 		}
 		return result;
 	}
+
+	public int getFrequentRenterPoint(Rental daysRented) {
+		int frequentRenterPoints=1;
+		// add bonus for a two day new release rental
+		if ((daysRented.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+				&& daysRented.getDaysRented() > 1)
+			frequentRenterPoints++;
+		return frequentRenterPoints;
+	}
 }
